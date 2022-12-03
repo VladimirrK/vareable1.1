b781b6482e0216f4460a7e09f3379c23cf758bc5
VladimirrK <AngerPrideLust@gmail.com>
2022-12-03 23:34:09 +0500

    Сделал задания все задания

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
+        task7();
+        task8();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -58,5 +61,34 @@ public class Main {
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var boxers = boxer1 + boxer2;
+        System.out.println("Общий вес двух бойцов"+ boxers +"кг");
+        var overload = (boxer2 - boxer1);
+        System.out.println("Разница между весами бойцов" + overload +"кг");
+    }
+    public static void task7() {
+        System.out.println("Задача 7");
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var overload = (boxer2 - boxer1);
+        System.out.println("Разница между весами бойцов" + overload +"кг");
+        var overload2 = (boxer2 % boxer1);
+        System.out.println("Разница между весами бойцов" + overload2 +"кг");
+    }
+    public static void task8() {
+        System.out.println("Задача 8");
+        var allTime = 640;
+        var workingDay = 20;
+        var time = 8;
+        var workers = (allTime / workingDay) / time;
+        System.out.println("Всего работников в компании" + workers + "человека");
+        var workers2 = workers + 94;
+        var allTime2 = workers2 * time * workingDay;
+        System.out.println("Если в компании работает" + workers2 + "человек, то всего" + allTime2 + "часов работы может быть поделено между сотрудниками");
     }
 }
